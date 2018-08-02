1216d88c9290b9577370de008abf283c7e7cbda5
Lee Burch <ltburch2000@gmail.com>
2018-08-02 16:34:49 -0500

    Modify the routes to not append

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -11,16 +11,18 @@ import { CustomerServiceComponent } from './customer-service/customer-service.co
 
 //  { path: 'login', component: LoginComponent },
 const routes: Routes = [
-  { path: 'dashboard', component: DashboardComponent, canActivate: [AuthGuard]},
+  { path: '',   redirectTo: 'dashboard', pathMatch: 'full' },
+  { path: 'dashboard', component: DashboardComponent, canActivate: [AuthGuard] },
   { path: 'billing', component: BillingComponent, canActivate: [AuthGuard] },
   { path: 'usage', component:  UsageComponent, canActivate: [AuthGuard] },
   { path: 'profile', component: ProfileComponent, canActivate: [AuthGuard] },
   { path: 'customer-service', component: CustomerServiceComponent, canActivate: [AuthGuard] },
-  { path: '', redirectTo: '/dashboard',  pathMatch: 'full'}
+  // route unrecognized back to dashboard
+  { path: '**', redirectTo: 'dashboard' }
 ];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forRoot(routes,  { enableTracing: true,   useHash: true } ) ],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
