bf7678adc52071ff20d33955aacbb94a9c3ff6ff
harish bhavanichikar <harish.bhavanichikar@perficient.com>
2018-08-07 22:45:23 +0530

    add snackbar component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,7 +7,7 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AppNavComponent } from './app-nav/app-nav.component';
 import { LayoutModule } from '@angular/cdk/layout';
 import { MatToolbarModule, MatButtonModule, MatCardModule, MatInputModule, MatFormFieldModule,
-         MatSidenavModule, MatExpansionModule, MatTabsModule, MatIconModule, MatListModule, MatTableModule, MatPaginatorModule, MatSortModule } from '@angular/material';
+         MatSidenavModule, MatSnackBarModule, MatExpansionModule, MatTabsModule, MatIconModule, MatListModule, MatTableModule, MatPaginatorModule, MatSortModule } from '@angular/material';
 import { DashboardComponent } from './dashboard/dashboard.component';
 import { BillingComponent } from './billing/billing.component';
 import { UsageComponent } from './usage/usage.component';
@@ -51,7 +51,8 @@ import { Logger } from './util/logger.service';
     MatPaginatorModule,
     MatSortModule,
     MatTabsModule,
-    MatExpansionModule
+    MatExpansionModule,
+    MatSnackBarModule
   ],
   providers: [
     Logger,
--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { FormControl, FormBuilder, FormGroup, Validators } from '@angular/forms';
+import {MatSnackBar} from '@angular/material';
 
 @Component({
   selector: 'dashboard',
@@ -8,15 +9,23 @@ import { FormControl, FormBuilder, FormGroup, Validators } from '@angular/forms'
 })
 export class DashboardComponent implements OnInit {
 
-
   ngOnInit() {
 
   }
   email = new FormControl('', [Validators.required, Validators.email]);
+
 
   getErrorMessage() {
     return this.email.hasError('required') ? 'You must enter a value' :
         this.email.hasError('email') ? 'Not a valid email' :
             '';
   }
+
+  constructor(public snackBar: MatSnackBar) {}
+
+   openSnackBar(message: string, action: string) {
+     this.snackBar.open(message, action, {
+       duration: 2000,
+     });
+   }
 }
