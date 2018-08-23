e580bafab8f5dc58fdd37cacc387f34bb14b2f16
Lee Burch <ltburch2000@gmail.com>
2018-08-23 10:51:59 -0500

    Smarter entirex dummy service

--- src/app/services/entirex-service.ts
+++ src/app/services/entirex-service.ts
@@ -1,75 +1,25 @@
 import { Observable, of } from 'rxjs';
 import { plainToClass } from 'class-transformer';
 import { AccountInfo } from '../models/account-info';
-import { Address } from '../models/address';
 import { AccountPremiseInfo } from '../models/account-premise-info';
 import { delay } from 'rxjs/internal/operators';
+import { EntireXDummyData } from '../dummydata/entirex';
+import { Logger } from '../util/logger.service';
 
 export class EntireXService {
-  dummyAccountInfo = '{ \
-    \"returnCode\": 0, \
-    \"messageCode\": 0, \
-    \"message\": \"Successful response\", \
-    \"custId\": \"468143\", \
-    \"utilsAcnt\": 2, \
-    \"nutsAcnt\": 0, \
-    \"depOnHand\": 0.00, \
-    \"totalAmtDue\": 129.48, \
-    \"utilAmtDue\": 129.48, \
-    \"nutAmtD\": 0.00, \
-    \"currAmtDue\": 129.48, \
-    \"utilCamTd\": 129.48, \
-    \"nutCamTd\": 0.00, \
-    \"utilArs030\": 129.48, \
-    \"utilArs3160\": 0.00, \
-    \"utilArs6190\": 0.00, \
-    \"utilArs91UP\": 0.00, \
-    \"nutArs030\": 0.00, \
-    \"nutArs3160\": 0.00, \
-    \"nutArs6190\": 0.00, \
-    \"nutArs91UP\": 0.00, \
-    \"writeOff\": 0.00, \
-    \"utilWriteOff\": 0.00, \
-    \"nutWriteOff\": 0.00, \
-    \"enfInd\": \"E\", \
-    \"premiseCnt\": 1, \
-    \"billCycle\": \"10\", \
-    \"daysTillDue\": 20, \
-    \"totalBudgetAmt\": 0.00, \
-    \"ruleId\": \"4529371376\", \
-    \"legalOperEntity\": \"CLFP\", \
-    \"startDt\": \"0\", \
-    \"stopDt\": \"0\", \
-    \"sendToBank\": \"0\", \
-    \"postDt\": \"0\", \
-    \"amount\": 0.00, \
-    \"oneTimeInd\": \"Y\", \
-    \"eBillStatus\": \"N\" \
-  }';
+  constructor (
 
-  dummyPremiseInfo = ' { \
-    "returnCode" : 0, \
-    "messageCode" : 0, \
-    "numberReturned" : 1, \
-    "accountId" : "0000082350", \
-    "lastName" : \"HERDER\", \
-    "firstName" : \"DELORES\", \
-    "premiseInfo" : [ { \
-      \"address1\" : \"DELORES HERDER\", \
-      \"address2\" : \"741 SPRINGFIELD TERRACE\", \
-      \"address3\" : \"CHEYENNE WY 82007 3521\" \
-    }] \
-  }';
-
+  ) { }
 
   public getAccountInfo(accountId: string): Observable<AccountInfo> {
-    const rv = plainToClass(AccountInfo, JSON.parse(this.dummyAccountInfo) as AccountInfo);
+    console.log(accountId + ' - ' + EntireXDummyData.dummyAccountInfoMap.get(accountId));
+    const rv = plainToClass(AccountInfo, JSON.parse(EntireXDummyData.dummyAccountInfoMap.get(accountId) ) as AccountInfo);
     return of( rv ).pipe( delay(1000) ) ;
 
   }
 
   public getAccountPremiseInfo(accountId: string): Observable<AccountPremiseInfo> {
-    const rv = plainToClass(AccountPremiseInfo, JSON.parse(this.dummyPremiseInfo) as AccountPremiseInfo);
+    const rv = plainToClass(AccountPremiseInfo, JSON.parse(EntireXDummyData.dummyPremiseInfoMap.get(accountId)) as AccountPremiseInfo);
     return of( rv ).pipe( delay(1000) );
 
   }
