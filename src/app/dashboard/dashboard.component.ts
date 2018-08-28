449ec7e28f596fa6651406dec201fe62a386ec9c
harish bhavanichikar <harish.bhavanichikar@perficient.com>
2018-08-28 16:21:05 -0500

    dashboard page components

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -1,11 +1,14 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, DoCheck } from '@angular/core';
 import { FormControl, FormBuilder, FormGroup, Validators } from '@angular/forms';
 import {MatSnackBar} from '@angular/material';
 // NavbarService is used to show/hide the secondary navigation
 import { NavbarService } from '../services/navbar.service';
+import { UserDetailsService } from '../services/user-details.service';
 //import { MenuService } from '../services/navbar.service';
 import { BHUserResolve } from '../util/user-resolve';
+import { BHUser } from '../models/bh-user';
 import { Logger } from '../util/logger.service';
+import { Global } from '../util/global';
 // import {MatDialog, MatDialogRef} from '@angular/material';
 // import { AccountSelectorComponent } from '../account-selector/account-selector.component';
 
@@ -15,9 +18,11 @@ import { Logger } from '../util/logger.service';
   templateUrl: './dashboard.component.html',
   styleUrls: ['./dashboard.component.css']
 })
-export class DashboardComponent implements OnInit {
+export class DashboardComponent implements DoCheck, OnInit {
   email = new FormControl('', [Validators.required, Validators.email]);
   hide = true; // property for form element
+  bhUser: BHUser = null;
+
 
 
   // constructor(public snackBar: MatSnackBar, private dialog: MatDialog) {}
@@ -37,6 +42,12 @@ export class DashboardComponent implements OnInit {
       //this.menu.show(); // show hamburger menu
 
     });
+  }
+
+  ngDoCheck() {
+    // this.logger.log('init nav');
+    // a little hacky and wasteful but very light
+    this.bhUser = Global.currentUser;
   }
 
 
